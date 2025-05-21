a467ee98507d9bebf55dfa6c2ecc77d2c2f1bdea
n1meshh <nimeshadhikari1216@gmail.com>
2025-05-21 14:50:07 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Guess-the_Number_Game
---
+++ script.js
@@ -0,0 +1,9 @@
+
+/*  suru ma euta random number generate garne  */
+
+const randomNumber = Math.floor(Math.random()*100)+1;
+console.log(randomNumber);
+
+
+
+
